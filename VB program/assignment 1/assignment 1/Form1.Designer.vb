ca7c2a4af87a81fbe24bd5c0efb954b1da62895c
joelp53 <joelparkinson@live.co.uk>
2015-05-08 02:31:14 +0100

    Added VB Program

---
+++ VB program/assignment 1/assignment 1/Form1.Designer.vb
@@ -0,0 +1,277 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(form1))
+        Me.MenuStrip = New System.Windows.Forms.MenuStrip
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.InformationToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.ResetToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.QuitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.lblName = New System.Windows.Forms.Label
+        Me.txtbPass = New System.Windows.Forms.TextBox
+        Me.lblPass = New System.Windows.Forms.Label
+        Me.lblMerit = New System.Windows.Forms.Label
+        Me.lblDistinctions = New System.Windows.Forms.Label
+        Me.txtbMerit = New System.Windows.Forms.TextBox
+        Me.txtbDist = New System.Windows.Forms.TextBox
+        Me.lblGradeTxt = New System.Windows.Forms.Label
+        Me.lblResult = New System.Windows.Forms.Label
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox
+        Me.imgList = New System.Windows.Forms.ImageList(Me.components)
+        Me.btnCalculate = New System.Windows.Forms.Button
+        Me.lblRemainingtxt = New System.Windows.Forms.Label
+        Me.lblRemaining = New System.Windows.Forms.Label
+        Me.MenuStrip.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MenuStrip
+        '
+        Me.MenuStrip.AutoSize = False
+        Me.MenuStrip.BackColor = System.Drawing.SystemColors.Control
+        Me.MenuStrip.Dock = System.Windows.Forms.DockStyle.None
+        Me.MenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.QuitToolStripMenuItem})
+        Me.MenuStrip.Location = New System.Drawing.Point(9, 0)
+        Me.MenuStrip.Name = "MenuStrip"
+        Me.MenuStrip.Size = New System.Drawing.Size(476, 24)
+        Me.MenuStrip.TabIndex = 0
+        Me.MenuStrip.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.InformationToolStripMenuItem, Me.HelpToolStripMenuItem, Me.ResetToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'InformationToolStripMenuItem
+        '
+        Me.InformationToolStripMenuItem.Name = "InformationToolStripMenuItem"
+        Me.InformationToolStripMenuItem.Size = New System.Drawing.Size(137, 22)
+        Me.InformationToolStripMenuItem.Text = "Information"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(137, 22)
+        Me.HelpToolStripMenuItem.Text = "Help"
+        '
+        'ResetToolStripMenuItem
+        '
+        Me.ResetToolStripMenuItem.Name = "ResetToolStripMenuItem"
+        Me.ResetToolStripMenuItem.Size = New System.Drawing.Size(137, 22)
+        Me.ResetToolStripMenuItem.Text = "Reset"
+        '
+        'QuitToolStripMenuItem
+        '
+        Me.QuitToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.QuitToolStripMenuItem.AutoSize = False
+        Me.QuitToolStripMenuItem.Name = "QuitToolStripMenuItem"
+        Me.QuitToolStripMenuItem.Size = New System.Drawing.Size(42, 20)
+        Me.QuitToolStripMenuItem.Text = "Quit"
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblName.Location = New System.Drawing.Point(4, 38)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(487, 29)
+        Me.lblName.TabIndex = 1
+        Me.lblName.Text = "LEVEL 3 BTEC GRADING CALCULATOR"
+        '
+        'txtbPass
+        '
+        Me.txtbPass.Location = New System.Drawing.Point(30, 106)
+        Me.txtbPass.Name = "txtbPass"
+        Me.txtbPass.Size = New System.Drawing.Size(140, 20)
+        Me.txtbPass.TabIndex = 2
+        '
+        'lblPass
+        '
+        Me.lblPass.AutoSize = True
+        Me.lblPass.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPass.Location = New System.Drawing.Point(27, 85)
+        Me.lblPass.Name = "lblPass"
+        Me.lblPass.Size = New System.Drawing.Size(110, 18)
+        Me.lblPass.TabIndex = 3
+        Me.lblPass.Text = "Nº Of Passes"
+        '
+        'lblMerit
+        '
+        Me.lblMerit.AutoSize = True
+        Me.lblMerit.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMerit.Location = New System.Drawing.Point(27, 140)
+        Me.lblMerit.Name = "lblMerit"
+        Me.lblMerit.Size = New System.Drawing.Size(101, 18)
+        Me.lblMerit.TabIndex = 4
+        Me.lblMerit.Text = "Nº Of Merits"
+        '
+        'lblDistinctions
+        '
+        Me.lblDistinctions.AutoSize = True
+        Me.lblDistinctions.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDistinctions.Location = New System.Drawing.Point(27, 213)
+        Me.lblDistinctions.Name = "lblDistinctions"
+        Me.lblDistinctions.Size = New System.Drawing.Size(134, 18)
+        Me.lblDistinctions.TabIndex = 5
+        Me.lblDistinctions.Text = "Nº Of Distinction"
+        '
+        'txtbMerit
+        '
+        Me.txtbMerit.Location = New System.Drawing.Point(30, 161)
+        Me.txtbMerit.Name = "txtbMerit"
+        Me.txtbMerit.Size = New System.Drawing.Size(140, 20)
+        Me.txtbMerit.TabIndex = 6
+        '
+        'txtbDist
+        '
+        Me.txtbDist.Location = New System.Drawing.Point(30, 234)
+        Me.txtbDist.Name = "txtbDist"
+        Me.txtbDist.Size = New System.Drawing.Size(140, 20)
+        Me.txtbDist.TabIndex = 7
+        '
+        'lblGradeTxt
+        '
+        Me.lblGradeTxt.AutoSize = True
+        Me.lblGradeTxt.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblGradeTxt.Location = New System.Drawing.Point(253, 131)
+        Me.lblGradeTxt.Name = "lblGradeTxt"
+        Me.lblGradeTxt.Size = New System.Drawing.Size(108, 16)
+        Me.lblGradeTxt.TabIndex = 8
+        Me.lblGradeTxt.Text = "Current Grade:"
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = True
+        Me.lblResult.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblResult.Location = New System.Drawing.Point(367, 131)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(90, 16)
+        Me.lblResult.TabIndex = 9
+        Me.lblResult.Text = "Not Entered"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(256, 161)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(201, 149)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 10
+        Me.PictureBox1.TabStop = False
+        '
+        'imgList
+        '
+        Me.imgList.ImageStream = CType(resources.GetObject("imgList.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.imgList.TransparentColor = System.Drawing.Color.Transparent
+        Me.imgList.Images.SetKeyName(0, "neutral.png")
+        Me.imgList.Images.SetKeyName(1, "smiley.png")
+        Me.imgList.Images.SetKeyName(2, "happy.png")
+        Me.imgList.Images.SetKeyName(3, "likeasir.jpg")
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(30, 274)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(140, 36)
+        Me.btnCalculate.TabIndex = 11
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'lblRemainingtxt
+        '
+        Me.lblRemainingtxt.AutoSize = True
+        Me.lblRemainingtxt.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblRemainingtxt.Location = New System.Drawing.Point(181, 85)
+        Me.lblRemainingtxt.Name = "lblRemainingtxt"
+        Me.lblRemainingtxt.Size = New System.Drawing.Size(250, 18)
+        Me.lblRemainingtxt.TabIndex = 12
+        Me.lblRemainingtxt.Text = "Not enough entered. Remaining:"
+        Me.lblRemainingtxt.Visible = False
+        '
+        'lblRemaining
+        '
+        Me.lblRemaining.AutoSize = True
+        Me.lblRemaining.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblRemaining.Location = New System.Drawing.Point(437, 87)
+        Me.lblRemaining.Name = "lblRemaining"
+        Me.lblRemaining.Size = New System.Drawing.Size(16, 16)
+        Me.lblRemaining.TabIndex = 13
+        Me.lblRemaining.Text = "0"
+        Me.lblRemaining.Visible = False
+        '
+        'form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(494, 332)
+        Me.Controls.Add(Me.lblRemaining)
+        Me.Controls.Add(Me.lblRemainingtxt)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.lblGradeTxt)
+        Me.Controls.Add(Me.txtbDist)
+        Me.Controls.Add(Me.txtbMerit)
+        Me.Controls.Add(Me.lblDistinctions)
+        Me.Controls.Add(Me.lblMerit)
+        Me.Controls.Add(Me.lblPass)
+        Me.Controls.Add(Me.txtbPass)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.MenuStrip)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip
+        Me.Name = "form1"
+        Me.Text = "BTEC Grading Calculator"
+        Me.MenuStrip.ResumeLayout(False)
+        Me.MenuStrip.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents InformationToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ResetToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents QuitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents lblName As System.Windows.Forms.Label
+    Friend WithEvents txtbPass As System.Windows.Forms.TextBox
+    Friend WithEvents lblPass As System.Windows.Forms.Label
+    Friend WithEvents lblMerit As System.Windows.Forms.Label
+    Friend WithEvents lblDistinctions As System.Windows.Forms.Label
+    Friend WithEvents txtbMerit As System.Windows.Forms.TextBox
+    Friend WithEvents txtbDist As System.Windows.Forms.TextBox
+    Friend WithEvents lblGradeTxt As System.Windows.Forms.Label
+    Friend WithEvents lblResult As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents imgList As System.Windows.Forms.ImageList
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+    Friend WithEvents lblRemainingtxt As System.Windows.Forms.Label
+    Friend WithEvents lblRemaining As System.Windows.Forms.Label
+
+End Class
---
+++ VB program/assignment 1/assignment 1/Form1.vb
@@ -0,0 +1,226 @@
+﻿'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+'x          Programmed by: Joel Parkinson              x
+'x           Last Updated: 11th April 2015             x
+'x           Program Name: BTEC Grading Calculator     x
+'x    Purpose: BTEC lvl 3: Programming Assignment      x
+'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+Public Class form1
+    Dim info As String = "This is the BTEC Level 3 Grading Calculator Version 6.0. Created by Joel Parkinson"
+    Dim checker As Int32 = 0                'Varibale is used for cleanup, DO NOT EDIT OR REMOVE!
+
+
+    'Public variables (Can be edited if desired)
+#Region "Public Editable Variables"
+    '                     !!Editable section!!
+    'This section is where you can set each variable for the program.
+    'Each one can be set to an integer(number) that corisponds to the years value for each.
+    'Example: If you set unitValue to 20 instead of 10, that would mean the value per unit used in the calculation would also be 20.
+    'This same rule applies for all other variables in this section.
+    Dim unitValue As Int32 = 10             '< -- Variable for unit value
+    Dim pass As Int32 = 7                   '< -- Variable for pass value
+    Dim merit As Int32 = 8                  '< -- Variable for merit value
+    Dim dist As Int32 = 9                   '< -- Variable for distinction value
+    Dim numUnits As Int32 = 18              '< -- Variable for number of units required to be entered
+
+    'This section is to set the minimum and maximum points for each grade.
+    'Example: If the required points for Pass Pass Pass changes from anything between 1260 and 1299 (Current default) then you would set the minPPP to the current 
+    'required minimum points for PPP and the maxPP to the maximum cap for PPP.
+#Region "Min/Max points required per grade"
+    'PPP
+    Dim minPPP As Int32 = 1260
+    Dim maxPPP As Int32 = 1299
+    'MPP
+    Dim minMPP As Int32 = 1300
+    Dim maxMPP As Int32 = 1339
+    'MMP
+    Dim minMMP As Int32 = 1340
+    Dim maxMMP As Int32 = 1379
+    'MMM
+    Dim minMMM As Int32 = 1380
+    Dim maxMMM As Int32 = 1419
+    'DMM
+    Dim minDMM As Int32 = 1420
+    Dim maxDMM As Int32 = 1459
+    'DDM
+    Dim minDDM As Int32 = 1460
+    Dim maxDDM As Int32 = 1499
+    'DDD
+    Dim minDDD As Int32 = 1500
+    Dim maxDDD As Int32 = 1529
+    'D*DD
+    Dim minDsDD As Int32 = 1530
+    Dim maxDsDD As Int32 = 1559
+    'D*D*D
+    Dim minDsDsD As Int32 = 1560
+    Dim maxDsDsD As Int32 = 1589
+    'D*D*D*
+    Dim DsDsDs As Int32 = 1590
+#End Region
+
+    '                    !!End of editable section!!
+#End Region
+
+
+
+    'Please don't edit below this line if you don't know what you're doing.
+
+    'Calculate button region
+#Region "Button - Calculate"
+    'On click event (Do not edit this!)
+    Private Sub btnCalculate_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCalculate.Click
+        check()
+    End Sub
+#End Region
+
+    'Check function region
+#Region "Function - check"
+    'This is where all the magic happens
+    'Sub that Calculates results and checks if the amount of units entered entered is above, below or equal to the required 18 units.
+    Private Sub check()
+
+        Dim result, remaining As Int32
+        remaining = Val(txtbPass.Text) + Val(txtbMerit.Text) + Val(txtbDist.Text)                       '< -- Variables for amount of units entered
+        'Calculates result using the calc function
+        result = calc(Val(txtbPass.Text), Val(txtbMerit.Text), Val(txtbDist.Text), unitValue)
+        'If statement checks if units entered are below 18
+        If remaining = numUnits Then
+            lblRemainingtxt.Visible = False
+            Select Case result                  '< -- Select case statement checks the result calculation against what grade a person should get
+                Case minPPP To maxPPP
+                    checker = 1
+                    lblResult.Text = "PPP"
+                    PictureBox1.Image = imgList.Images.Item(0)
+                Case minMPP To maxMPP
+                    checker = 1
+                    lblResult.Text = "MPP"
+                    PictureBox1.Image = imgList.Images.Item(1)
+                Case minMMP To maxMMP
+                    checker = 1
+                    lblResult.Text = "MMP"
+                    PictureBox1.Image = imgList.Images.Item(1)
+                Case minMMM To maxMMM
+                    checker = 1
+                    lblResult.Text = "MMM"
+                    PictureBox1.Image = imgList.Images.Item(1)
+                Case minDMM To maxDMM
+                    checker = 1
+                    lblResult.Text = "DMM"
+                    PictureBox1.Image = imgList.Images.Item(2)
+                Case minDDM To maxDDM
+                    checker = 1
+                    lblResult.Text = "DDM"
+                    PictureBox1.Image = imgList.Images.Item(2)
+                Case minDDD To maxDDD
+                    checker = 1
+                    lblResult.Text = "DDD"
+                    PictureBox1.Image = imgList.Images.Item(2)
+                Case minDsDD To maxDsDD
+                    checker = 1
+                    lblResult.Text = "D* DD"
+                    PictureBox1.Image = imgList.Images.Item(2)
+                Case minDsDsD To maxDsDsD
+                    checker = 1
+                    lblResult.Text = "D* D* D"
+                    PictureBox1.Image = imgList.Images.Item(3)
+                Case Is >= DsDsDs
+                    checker = 1
+                    lblResult.Text = "D* D* D*"
+                    PictureBox1.Image = imgList.Images.Item(3)
+            End Select
+        ElseIf remaining > numUnits Then                                        '<-- This triggers if the ammount of units entered is more than the required amount.
+            lblRemaining.Visible = False
+            lblRemainingtxt.Visible = True
+            lblRemainingtxt.Text = "Too many entered"
+            checker = 2
+        ElseIf remaining < numUnits Then                                        '<-- This triggers if the ammount of units entered is less than the required amount.
+            lblRemainingtxt.Visible = True
+            lblRemaining.Visible = True
+            lblRemainingtxt.Text = "Not enough entered. Remaining: "
+            lblRemaining.Text = Convert.ToString(numUnits - remaining)
+            checker = 4
+        End If
+        clear()
+
+    End Sub
+#End Region
+
+
+    'Quits application when quit is clicked (Do not edit this!)
+#Region "Quit"
+    Private Sub QuitToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles QuitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+#End Region
+
+
+    'Resets form (Do not edit this!)
+#Region "Reset"
+    Private Sub ResetToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ResetToolStripMenuItem.Click
+        checker = 3
+        clear()
+    End Sub
+#End Region
+
+    'Information button, open messagebox to display program information (Do not edit this!)
+#Region "Information"
+    Private Sub InformationToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles InformationToolStripMenuItem.Click
+        MessageBox.Show(info)
+    End Sub
+#End Region
+
+    'Shows a help dialog when the help button is pressed.
+#Region "Help"
+    Private Sub HelpToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles HelpToolStripMenuItem.Click
+        Dim msg_help = "To use this program you simply enter the number of passes, merits, distinctions the student or yourself atained into the defined text boxes. Was this helpful?"
+        Dim msg_yes = "I am glad I could help! - Joel Parkinson"
+        Dim msg_no = "If you need more help please contact joelparkinson@live.co.uk or seek help from a colegue/teacher"
+        Dim title = "Help Dialog"
+        Dim style = MsgBoxStyle.YesNo Or MsgBoxStyle.DefaultButton1 Or MsgBoxStyle.Information
+        Dim response = MsgBox(msg_help, style, title)
+        If response = MsgBoxResult.Yes Then
+            MsgBox(msg_yes, , title)
+        Else
+            MsgBox(msg_no, , title)
+        End If
+    End Sub
+#End Region
+
+    'This is where my functions are stored for the program to run correctly.
+#Region "Functions"
+
+    'Funtion to calculate amount of points awarded to pupil (Do not edit! Values are set at the top in the variable section!!)
+    Function calc(ByVal p As Int32, ByVal m As Int32, ByVal d As Int32, ByVal value As Int32) As Int32
+        Return (pass * p * value) + (merit * m * value) + (dist * d * value)
+    End Function
+
+    'Sub to clear text boxes based on which needs to be cleared (Do not edit unless more clear functions need adding!)
+    Private Sub clear()
+        Select Case checker
+            Case 1                                      '<-- Clears unused items when the program is reused without resetting (sort of a debug feature).
+                lblRemaining.Visible = False
+                checker = 0
+            Case 2                                      '<-- Clear function to clear unused items when the number of units entered is more than what is required.
+                PictureBox1.Image = Nothing
+                lblResult.Text = "Not Entered"
+                checker = 0
+            Case 3                                      '<-- Clear function when reset is clicked
+                txtbPass.Text = ""
+                txtbMerit.Text = ""
+                txtbDist.Text = ""
+                PictureBox1.Image = Nothing
+                lblRemainingtxt.Visible = False
+                lblRemaining.Visible = False
+                lblResult.Text = "Not Entered"
+                checker = 0
+            Case 4                                      '<-- Clear function to clear unused items when the number of units entered is less than what is required.
+                PictureBox1.Image = Nothing
+                lblResult.Text = "Not Entered"
+                checker = 0
+        End Select
+    End Sub
+
+#End Region
+
+
+End Class
